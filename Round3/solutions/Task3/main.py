5f25f52b2e80281e1e5a494c32a070e5aeca87fb
ngxquang <nq23092016@gmail.com>
2023-06-06 23:25:36 +0700

    Refactor and add functions' description

--- Round3/solutions/Task3/main.py
+++ Round3/solutions/Task3/main.py
@@ -7,7 +7,10 @@ from utils import load_config
 def init_arguments():
     """
     This function is used to get arguments from the command line.
-    -i, --input_file flags are used the path to config file
+    .. Flags:
+        :-i, --input_file: path to config file
+    .. Returns:
+        :args.input_file: path to config yaml
     """
     parser = argparse.ArgumentParser()
 
@@ -22,41 +25,97 @@ def init_arguments():
     return args.input_file
 
 
+def retrieve_SHA(url, headers, branch, filename):
+    """
+    Query GitHub to retreive SHA for file.
+    If file exists on the branch, then return its current SHA
+    If file does note exist, the SHA will be None
+
+    .. Args:
+        :url: GitHub API URL to access to a repo
+        :headers: request headers, include the access token
+        :branch: the name of the branch to access, defined in the config yaml.
+        :filename: the name of the file we want to push/overwrite, defined in the config.
+    .. Returns:
+        :SHA: sha value corresponding to the file.
+
+    """
+    url += filename
+    response = requests.get(url, headers, data={"branch": branch})
+    if response.status_code == 200:
+        # Response message is OK, file exists and can extract SHA
+        sha = response.json()["sha"]
+    else:
+        # file does not exist in the repo, ignore the SHA value.
+        sha = None
+    return sha
+
+
+def upload(url, headers, branch, message, filename, sha=None):
+    """
+    Push the file to GitHub and overwrite if the file exists
+
+    .. Args:
+        :url: GitHub API URL to access to the repo
+        :headers: request headers, include the access token
+        :branch: the name of the branch to upload, defined in the config file
+        :message: commit message, defined in the config file
+        :filename: the name of the file to upload, defined in the config file
+        :sha: the current SHA value of the file.
+    .. Outputs:
+        :200: if file exists and overwrite it successfully
+        :201: if file doesnot exist and push it successfully
+        :401: action fail
+    """
+    url += filename
+    with open(filename, "rb") as f:
+        encoded_data = base64.b64encode(
+            f.read()
+        )  # file's content must be written under the base64 format
+
+        data = {
+            "branch": branch,
+            "message": message,
+            "content": encoded_data.decode("utf-8"),  # content of the commited file
+        }
+        # if file exist, assign file's SHA as an attribute, else ignore
+        if sha is not None:
+            data["sha"] = sha
+
+        response = requests.put(url, headers=headers, json=data)
+        if response.status_code == 200:
+            print("200 - Overwrite successfully!")
+        elif response.status_code == 201:
+            print("201 - Push a new file successfully!")
+        else:
+            print("Overwrite fail!")
+            print("Message: {}".format(response.json()["message"]))
+
+
 if __name__ == "__main__":
-    config_file_path = init_arguments()
+    INP_CONFIG = init_arguments()
 
-    config = load_config(config_file_path)
-    assert config is not None  # To ensure config_file_path is valid
+    config = load_config(INP_CONFIG)
+    assert config is not None  # To ensure INP_CONFIG is valid
 
-    apiURL = f"https://api.github.com/repos/{config['REPOSITORY']['OWNER']}/{config['REPOSITORY']['NAME']}/contents/{config['FILENAME']}"
+    api_url = f"https://api.github.com/repos/{config['REPOSITORY']['OWNER']}/{config['REPOSITORY']['NAME']}/contents/"
     headers = {
         "Authorization": f"""Bearer {config['AUTHENTICATION']['TOKEN']}""",
         "Content-Type": "application/vnd.github+json",
     }
 
-    # Firstly, request to get file's SHA, if file does exist, the status_code will be 200.
-    data = {"branch": config["REPOSITORY"]["BRANCH"]}
-    response = requests.get(apiURL, headers=headers, json=data)
-    if response.status_code == 200:
-        sha = response.json()["content"]["sha"]
-    else:
-        sha = None
+    sha = retrieve_SHA(
+        api_url,
+        headers,
+        branch=config["REPOSITORY"]["BRANCH"],
+        filename=config["FILENAME"],
+    )
 
-    with open(config["FILENAME"], "rb") as f:
-        encoded_data = base64.b64encode(f.read())
-
-        data = {
-            "branch": config["REPOSITORY"]["BRANCH"],  # define the branch
-            "message": config["MESSAGE"],  # commit message
-            "content": encoded_data.decode("utf-8"),  # content of the commited file
-        }
-        if sha is not None:
-            data["sha"] = sha
-        # else if sha is none, the file does not exist in the repo
-
-        response = requests.put(apiURL, headers=headers, json=data)
-        if response == 200:
-            print("Overwrite successfully!")
-        else:
-            print("Overwrite fail!")
-            print(f"Message: {response.json()['message']}")
+    upload(
+        api_url,
+        headers,
+        branch=config["REPOSITORY"]["BRANCH"],
+        message=config["MESSAGE"],
+        filename=config["FILENAME"],
+        sha=sha,
+    )
